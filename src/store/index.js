f48158f9df063953dde8be584d9042940eb4df47
zhangguijun <1725779714@qq.com>
2020-02-27 17:42:23 +0800

    【feature】store 编写

---
+++ src/store/index.js
@@ -0,0 +1,61 @@
+import {
+  decorate,
+  observable,
+  computed,
+  action
+} from 'mobx';
+ 
+class TodoStore {
+  todos = [];
+  get unComputedTodosCount() {
+    return this.todos.filter(
+      todo => !todo.completed
+    ).length;
+  }
+  // 这里 的 computed 属性 简直就和 vue 的一样，对立面的值进行缓存，
+  // 当然，和 react:hook 中的 useMem 也一样就是了
+  get completedTodo() {
+    return this.todos.filter(
+      todo => todo.completed
+    )
+  }
+  get unCompletedTodo() {
+    return this.todos.filter(
+      todo => !todo.completed
+    )
+  }
+ 
+  addTodo(task) {
+    let id = 0;
+    if (this.todos.length !== 0) {
+      id = this.todos[this.todos.length - 1].id + 1
+    }
+    this.todos.push({
+      task: task,
+      completed: false,
+      id
+    });
+  }
+  deleteTodo(task) {
+    const index = this.todos.findIndex(todo => todo === task);
+    this.todos.splice(index, 1)
+  }
+  toggleTodo(task) {
+    task.completed = !task.completed
+  }
+}
+ 
+// 在这里对 数据进行包装， 注意 这里 包装的对象 必须是一个 类
+decorate(TodoStore, {
+  todos: observable,
+  unComputedTodosCount: computed,
+  completedTodo: computed,
+  unCompletedTodo: computed,
+  addTodo: action.bound,
+  deleteTodo: action.bound,
+  toggleTodo: action.bound,
+})
+ 
+const todoStore = new TodoStore();
+
+export default todoStore
